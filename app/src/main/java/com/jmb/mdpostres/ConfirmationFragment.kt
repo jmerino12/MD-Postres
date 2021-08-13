8fb37203cc7bd2b1d8665b58439214e9ebb8014d
Jonathan Meriño Bolivar <jmerino1204@gmail.com>
2021-08-13 06:06:54 -0500

    Action Confirmación a Productos

--- app/src/main/java/com/jmb/mdpostres/ConfirmationFragment.kt
+++ app/src/main/java/com/jmb/mdpostres/ConfirmationFragment.kt
@@ -5,16 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.jmb.mdpostres.databinding.FragmentConfirmationBinding
 
 class ConfirmationFragment : Fragment() {
-
+    private var _binding: FragmentConfirmationBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_confirmation, container, false)
+        _binding = FragmentConfirmationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnReady.setOnClickListener {
+            findNavController().navigate(R.id.action_confirmation_to_products)
+        }
     }
 
 }
