91ef1d73dfc9284297d970c38eb4ed08e3436245
Jonathan Meriño Bolivar <jmerino1204@gmail.com>
2021-08-13 06:31:38 -0500

    Global Action

--- app/src/main/java/com/jmb/mdpostres/MainActivity.kt
+++ app/src/main/java/com/jmb/mdpostres/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.jmb.mdpostres
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
@@ -10,6 +13,7 @@ import com.jmb.mdpostres.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,13 +24,25 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
         val configuration = AppBarConfiguration.Builder(navController.graph).build()
         NavigationUI.setupWithNavController(binding.toolbar, navController, configuration)
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
             binding.toolbar.title = destination.label
             binding.toolbar.navigationIcon = null
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_confirmation) {
+            navController.navigate(R.id.action_global_confirmation)
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onBackPressed() {
