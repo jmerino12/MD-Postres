1a0f55ac94f3764b1984ca30e9e66c48448aba15
Jonathan Meriño Bolivar <jmerino1204@gmail.com>
2021-08-13 06:18:08 -0500

    NavigationUI: Title & NavigationIcon

--- app/src/main/java/com/jmb/mdpostres/MainActivity.kt
+++ app/src/main/java/com/jmb/mdpostres/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.jmb.mdpostres
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.jmb.mdpostres.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val configuration = AppBarConfiguration.Builder(navController.graph).build()
+        NavigationUI.setupWithNavController(binding.toolbar, navController, configuration)
     }
 }
